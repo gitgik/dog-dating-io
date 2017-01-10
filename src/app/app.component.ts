72aea774eecf0b5a150bb82640a4e518f72e3523
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-10 21:56:37 +0300

    (chore) add deps for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdButton } from '@angular2-material/button';
+// import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+// import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'dog dating io works!';
 }
