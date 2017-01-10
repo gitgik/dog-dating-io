ef12a68b27fe1c828d8eff826a220a83a87b91ea
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-10 22:40:04 +0300

    (chore) add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { MdToolbar } from '@angular2-material/toolbar';
-import { MdButton } from '@angular2-material/button';
+// import { MdToolbar } from '@angular2-material/toolbar';
+// import { MdButton } from '@angular2-material/button';
+// import { APP_SHELL_DIRECTIVES } from '@angular/app-shell';
 // import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 // import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
 import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
@@ -9,7 +10,6 @@ import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
 })
 export class AppComponent {
   title = 'dog dating io works!';
