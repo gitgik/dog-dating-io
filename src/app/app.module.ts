1d827f4308aec44a3db05ae2311e6aa6de397cbf
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-11 11:05:30 +0300

    (chore) add hammerjs for touch gesture support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
